2f18cd358a4ce3a4b45016f738d5b6668ea4e3f0
Leonardo <leonardo.ranaldo.b@gmail.com>
2020-12-10 16:16:09 +0100

    push parte bonus

--- js/sctipt.js
+++ js/sctipt.js
@@ -2,12 +2,22 @@
 // - aggiungere un campo di input e un pulsante per aggiungere
 // nuove cose da fare alla lista;
 // - cancellare un item dalla lista, al click sulla relativa icona 'X'.
-
+// Bonus:
+// - Oltre al click sul pulsante, consentire l'inserimento del nuovo
+// item anche premendo il tasto Invio della tastiera, dopo aver scritto
+//  il testo nel campo di input.
+// Consiglio del giorno: per alcuni task, potrebbe essere necessario
+//  fare delle ricerche e capire come lavorano alcune funzioni o
+//  direttive leggendo
+// le documentazioni ufficiali ed analizzando i relativi esempi.
+// al click di un bottono parte un setInterval che fa il push di una nuovo
+// task ogni 5 secondi poi crea un pulsante per stopparolo
 let app = new Vue ({
   el: '#container',
   data: {
     tasks: ['fare la spesa', 'Guardare la partita di calcio', 'Chiamare medico', 'Andare dal meccanico'],
     newTask: '',
+    enter: 13,
   },
   methods: {
     newTaskPush: function() {
@@ -15,8 +25,12 @@ let app = new Vue ({
       this.newTask = '';
     },
     removeTask: function(i) {
-      console.log(this.tasks.splice(i, 1));
-      console.log(i);
+      this.tasks.splice(i, 1);
+    },
+    addtaskByKeycode:  function () {
+      if (event.which == this.enter) {
+        this.newTaskPush();
+      }
     }
   }
 })
